1ecec25e27cb1c137b84e8af1f34d8ff25aa47c4
abelmontea <87101653+abelmontea@users.noreply.github.com>
2021-08-09 14:44:58 -1000

    Cleaning up JS
    created universal variables

--- src/scripts.js
+++ src/scripts.js
@@ -1,16 +1,32 @@
+//universal variables
+let apiKey = "2e10dd656c312415a04f00d607cce045";
+let city = document.querySelector(".search-city");
+let cityDescription = document.querySelector("#weather-description");
+let currentTemp = document.querySelector("#current-temperature");
+let forecast = document.querySelector("#forecast-data");
+let forecastCity = document.querySelector("#forecast-city");
+let lowTemp = document.querySelector("#low-temp");
+let highTemp = document.querySelector("#high-temp");
+let humidity = document.querySelector("#humidity");
+let newCity = document.querySelector("#city-state");
+let weatherIcon = document.querySelector("#weather-icon");
+let wind = document.querySelector("#wind-speed");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 //upon loading weather
 function seattleWeather() {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=seattle&units=imperial&appid=2e10dd656c312415a04f00d607cce045`;
   axios.get(apiUrl).then(showSeattleWeather);
 }
 function showSeattleWeather(response) {
-  let cityDescription = document.querySelector("#weather-description");
-  let currentTemp = document.querySelector("#current-temperature");
-  let lowTemp = document.querySelector("#low-temp");
-  let highTemp = document.querySelector("#high-temp");
-  let humidity = document.querySelector("#humidity");
-  let weatherIcon = document.querySelector("#weather-icon");
-  let wind = document.querySelector("#wind-speed");
   let description = response.data.weather[0].main;
   let showHumidity = response.data.main.humidity;
   let showWind = response.data.wind.speed;
@@ -36,16 +52,6 @@ function showSeattleWeather(response) {
 seattleWeather();
 
 //date and time funcitonality
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
 let day = document.querySelector("#day");
 let now = new Date();
 
@@ -77,23 +83,14 @@ function showCurrentCity(event) {
   function showLocation(position) {
     let lat = Math.round(position.coords.latitude);
     let long = Math.round(position.coords.longitude);
-    let apiKey = "2e10dd656c312415a04f00d607cce045";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=${apiKey}`;
     axios.get(apiUrl).then(changeCityandTemperature);
 
     let forecastApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`;
     axios.get(forecastApi).then(displayForecast);
   }
+
   function changeCityandTemperature(response) {
-    let cityDescription = document.querySelector("#weather-description");
-    let currentTemp = document.querySelector("#current-temperature");
-    let forecastCity = document.querySelector("#forecast-city");
-    let humidity = document.querySelector("#humidity");
-    let lowTemp = document.querySelector("#low-temp");
-    let highTemp = document.querySelector("#high-temp");
-    let newCity = document.querySelector("#city-state");
-    let weatherIcon = document.querySelector("#weather-icon");
-    let wind = document.querySelector("#wind-speed");
     let description = response.data.weather[0].main;
     let showHumidity = response.data.main.humidity;
     let showWind = response.data.wind.speed;
@@ -122,18 +119,11 @@ currentLocationButton.addEventListener("click", showCurrentCity);
 //Search location and temperature functionality
 function newCityTemperature(response) {
   let cityTemp = Math.round(response.data.main.temp);
-  let humidity = document.querySelector("#humidity");
   let showHumidity = response.data.main.humidity;
-  let currentTemp = document.querySelector("#current-temperature");
   let description = response.data.weather[0].main;
-  let cityDescription = document.querySelector("#weather-description");
-  let wind = document.querySelector("#wind-speed");
   let showWind = response.data.wind.speed;
-  let highTemp = document.querySelector("#high-temp");
-  let lowTemp = document.querySelector("#low-temp");
   let showHighTemp = Math.round(response.data.main.temp_max);
   let showLowTemp = Math.round(response.data.main.temp_min);
-  let weatherIcon = document.querySelector("#weather-icon");
 
   highTemp.innerHTML = `${showHighTemp}°`;
   lowTemp.innerHTML = `${showLowTemp}°`;
@@ -151,10 +141,7 @@ function newCityTemperature(response) {
 
 function showNewCity(event) {
   event.preventDefault();
-  let city = document.querySelector(".search-city");
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=imperial&appid=2e10dd656c312415a04f00d607cce045`;
-  let newCity = document.querySelector("#city-state");
-  let forecastCity = document.querySelector("#forecast-city");
   forecastCity.innerHTML = `${city.value}`;
   newCity.innerHTML = `${city.value} Weather`;
   axios.get(apiUrl).then(newCityTemperature);
@@ -165,7 +152,6 @@ searchForm.addEventListener("submit", showNewCity);
 
 //forecast functionality
 function displayForecast(response) {
-  let forecast = document.querySelector("#forecast-data");
   let forecastHTML = `<div class="row justify-content-start text-center">`;
   let forecastData = response.data.daily;
   forecastData.forEach(function (forecastDay, index) {
@@ -192,7 +178,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "2e10dd656c312415a04f00d607cce045";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -200,14 +185,5 @@ function getForecast(coordinates) {
 function formatTimestamp(date) {
   let timestamp = new Date(date * 1000);
   let day = timestamp.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
   return days[day];
 }
